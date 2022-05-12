0901804aa81422f68a753e6d622210c0528e9b19
Kiran Sharma <kiran.sharma@gurzu.com>
2022-05-12 10:51:19 +0545

    User interface implemented

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectDateButton = findViewById<Button>(R.id.button)
+        selectDateButton.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,DatePickerDialog.OnDateSetListener{view,year,month,dayOfMonth -> },
+        year,
+        month,
+        day).show()
+        Toast.makeText(this,"btnText picker pressed",Toast.LENGTH_LONG).show();
+
+
     }
 }
